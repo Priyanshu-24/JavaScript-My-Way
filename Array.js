8bcc6c9ee80e143a2ea4f15bd91a5ae74bb0ea69
Priyanshu Pandey <priyanshu24pandey@gmail.com>
2022-08-12 23:30:43 +0530

    JS Day - 5

---
+++ Array.js
@@ -0,0 +1,35 @@
+let arr = []; // array
+let obj = {}; // object
+
+console.log(typeof arr); // object
+console.log(typeof obj); // object
+
+console.log(Array.isArray(arr)); // true
+console.log(Array.isArray(obj)); // false
+
+// Array.isArray is used to check array
+
+// ways to clone array, as directly assigning value will not work for cloning(refer primitive vs reference file)
+
+let a = [1, 2, 3, 4, 5];
+let a1 = a;
+
+//method 1
+let b = a.slice(0);
+
+//method 2
+let c = [].concat(a);
+
+//method 3
+let d = [...a];
+
+a.push(6);
+a.push(7);
+
+console.log(a);
+console.log(a1);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// we can see that change in a doesnot effect b,c and d which was not the case with a1
